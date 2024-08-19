471e3f57ec240958e8890447c4305446adb6c3ea
0xkonsti <opora.privat@gmail.com>
2024-08-19 20:05:45 +0200

    some more work on wdt

--- src/bin/wdt.rs
+++ src/bin/wdt.rs
@@ -1,12 +1,15 @@
+use colored::Colorize;
 use std::path::Path;
 
-const DEFAULT_DEPTH: u32 = 2;
+const DEFAULT_DEPTH: u32 = 1;
+const PREFIX_FIRST: &str = "╭";
+const PREFIX_MIDDLE: &str = "├";
+const PREFIX_LAST: &str = "╰";
 
 struct WDTArgs<'a> {
     path: &'a Path,
     base_depth: u32,
     depth: u32,
-    hide: bool,
 }
 
 impl<'a> WDTArgs<'a> {
@@ -19,7 +22,6 @@ impl<'a> WDTArgs<'a> {
             path,
             base_depth: self.base_depth,
             depth: self.depth - 1,
-            hide: self.hide,
         }
     }
 }
@@ -30,9 +32,37 @@ impl Default for WDTArgs<'_> {
             path: Path::new("."),
             base_depth: DEFAULT_DEPTH,
             depth: DEFAULT_DEPTH,
-            hide: false,
         }
     }
+}
+
+fn get_prefix_symbol(indent: usize, index: usize, total: usize) -> String {
+    match (index, total, indent) {
+        (0, _, 0) => PREFIX_FIRST,
+        (i, t, _) if i == t - 1 => PREFIX_LAST,
+        (_, _, _) => PREFIX_MIDDLE,
+    }
+    .to_string()
+}
+
+fn format_name(path: &Path, name: &str, indent: usize, index: usize, total: usize, leaf: bool) -> String {
+    format!(
+        "{}{} {}",
+        "  ".repeat(indent),
+        get_prefix_symbol(indent, index, total).dimmed(),
+        if path.is_dir() {
+            format!(
+                "{}/",
+                if !leaf {
+                    name.dimmed().to_string()
+                } else {
+                    name.to_string()
+                }
+            )
+        } else {
+            name.to_string()
+        }
+    )
 }
 
 fn working_directory_tree(args: &WDTArgs) -> Result<(), String> {
@@ -45,19 +75,20 @@ fn working_directory_tree(args: &WDTArgs) -> Result<(), String> {
     }
 
     let dir = match args.path.read_dir() {
-        Ok(dir) => dir.filter_map(Result::ok).filter(|entry| {
-            entry
-                .path()
-                .file_name()
-                .is_some_and(|name| args.hide || !name.to_string_lossy().starts_with('.'))
-        }),
-        Err(e) => return Err(e.to_string()),
+        Ok(dir) => dir.filter_map(Result::ok),
+        Err(_) => return Ok(()), // skip over read acces fails
     };
 
-    for entry in dir {
+    let total = args.path.read_dir().unwrap().count();
+
+    for (i, entry) in dir.enumerate() {
         let path = entry.path();
         let name = path.file_name().unwrap().to_string_lossy();
-        println!("{}{}", "  ".repeat(args.indent()), name);
+
+        println!(
+            "{}",
+            format_name(&path, &name, args.indent(), i, total, args.depth == 1)
+        );
 
         if path.is_dir() {
             let new_args = args.go_deep(&path);
